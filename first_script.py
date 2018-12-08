4068d3f4776159f62df26b2f87d6f887264cb08a
limq-debianvm-home <gylimingqi@163.com>
2018-12-07 23:29:01 -0900

    first

---
+++ first_script.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from math import exp, log , sqrt
+print("output #11: {0:.4f}".format(exp(3)))
+print("output #12: {0:.2f}".format(log(4)))
+print("output #13: {0:.1f}".format(sqrt(81)))
