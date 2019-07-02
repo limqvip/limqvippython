8fa58c2720b8de680ab2e6a89f9779bede5a2cdd
gzlmq1 <gzlmq1@163.com>
2019-07-02 22:52:54 +0800

    bbc

---
+++ cc.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+def ddd()
+
+    
+    
+    class
+
+    class
+    print
+
+
+if
+
+for
+
+for
+class
+
+o
+class
+bbcabd
+
+bbcabd
+def
+
+
+print
+ddd
+
+class
+priprint
+    priprint
+    def 
+    
+    print("dddd")
