3ef3fc0394f68aac652e136da5b515ec7e98c62f
limq <gzlmq1@163.com>
2019-07-03 17:37:38 +0800

    2019-07-03 17:37:38  A  cprom/1-7.out A  cprom/a.out A  cprom/first.c A  cprom/first.out

---
+++ cprom/first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	int num;
+	num = 1;
+	printf("I am a simple");/*使用printf函数*/
+	printf(" computer.\n");
+	printf("My favorite number is %d because is is first.\n",num);
+	getchar();
+	return 0;
+}
