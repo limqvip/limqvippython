f72369f3458d20cc4ea437f9ea8461d11b32d06e
gzlmq1 <gzlmq1@163.com>
2019-07-09 00:00:43 +0800

    2019-07-09 00:00:43  A  cprom/badcount.c A  cprom/badcount.out A  cprom/escape.c A  cprom/escape.out A  cprom/talkback.c A  cprom/talkback.out A  cprom/vimtxt.pdf A  cprom/water.c A  cprom/water.out

---
+++ cprom/water.c
@@ -0,0 +1,15 @@
+/*water.c --计算水分子个数*/
+#include <stdio.h>
+int main(void)
+{
+	float water;
+	float gwater;
+	water = 3.156e-23;
+	printf("\aEnter water夸克:");
+	printf(" ________\b\b\b\b\b\b\b\b");
+	scanf ("%f",&gwater);
+	printf("\n\t$%.2f 夸水 是 $%.2f个水分子 .",gwater,(gwater * 950)/water );
+	printf("\rGee!\n");
+	
+	return 0;
+}
