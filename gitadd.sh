dca05af2681470ae22fe700461f58c934e23d20e
gzlmq1 <gzlmq1@163.com>
2019-07-20 14:22:50 +0800

    good

--- gitadd.sh
+++ gitadd.sh
@@ -1,7 +1,7 @@
 git status -s
 
 git status -s | awk ' $2 { print ( $2 ) }'>/home/limq/addgit
-git add `more /home/limq/addgit`
+git add `more /home/limqaddgit`
 git status -s
 git commit -m " `date "+%Y-%m-%d %H:%M:%S"`  `git status -s`"
 git status -s
