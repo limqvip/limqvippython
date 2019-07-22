57b24606b0689c48f3105f7b8a55d8dd8eac5cd9
limq <gzlmq1@163.com>
2019-07-22 13:23:22 +0800

    2019-07-22 13:23:22  A  running.c

---
+++ cprom/5/running.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+const int S_PER_M = 60;//1分钟的秒数
+const int S_PER_M = 3600;//1小时的分钟数
+const double M_PER_K = 0.62137;//1公里的英里数
+int main(void)
+{
+	
+	double distk, distm;//跑过的距离 分别以公里和英里为单位
+	double rate;//平均速度
+	int min, sec;//跑步用时
+	int time;//跑步用时-以秒为单位
+	double mtime;//跑一英里需要的时间，以秒为单位
+	int mmin, msec;//跑1英里需要的时间，以分钟和秒为单位
+
+	printf("This program converts your time for a metric race\n");
+	printf("to a time for running a mile and to your average\n");
+	printf("speed in miles per hour.\n");
+
+
+
+
+}
