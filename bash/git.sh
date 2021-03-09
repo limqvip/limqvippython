5905960f7ba36b19661812320c9993eaea929dac
gzlimq <gzlmq1@163.com>
2021-03-09 11:14:36 +0800

    2021-03-09 11:14:36   D 004.md  D Git_And_GitHub_Pull_Push.md  D LICENSE  M README.md  D a.bim  D aa.test A  bash/git.sh A  bash/gitadd.sh A  bash/gitmerge.sh A  bash/gitpush.sh A  bash/ssh.sh A  bash/wingitadd.sh A  docment/LICENSE A  docment/fengxian.md A  docment/sibiansiwei.md A  docment/softbook.md  D fengxian.md A  gitdocment/Git_And_GitHub_Pull_Push.md A  opt/sources.list  D sedhelp.md  D sibiansiwei.md  D softbook.md  D sources.list  D ssh.sh  D text.c  D vim  D vim.txt  D vim_gaoxiao.md  D vimhelp.md  D "vim\344\275\277\347\224\250"  D wingitadd.sh

---
+++ bash/git.sh
@@ -0,0 +1,11 @@
+
+git config --global user.name "limq-debian"
+git config --global user.email "gzlmq1@163.com"
+
+git config --global push.default simple
+#git remote add origin git@github.com:dadiqq/nework-git.git
+#git remote add origin git@github.com:dadiqq/nework-git.git
+#git remote add origin git@github.com:dadiqq/nework-git.git
+#git remote add origin git@github.com:dadiqq/nework-git.git
+#git remote add origin git@github.com:dadiqq/nework-git.git
+git remote add origin git@github.com:limqvip/limqvipcode.git
---
+++ bash/gitmerge.sh
@@ -0,0 +1,6 @@
+git fetch
+git diff origin/master
+echo 是否合并分支，合并请输入以下命令
+echo git merge origin
+echo git pull origin master pull与push相反，是将代码从远程仓库同步至本地仓库并merge的命令
+echo fetch 是单纯将代码从远程仓库同步至本地仓库，并不作merge。 gitfetchgit  diff origin/mastergit merge origin 
